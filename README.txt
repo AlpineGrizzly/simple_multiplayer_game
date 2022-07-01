81ae6cf5626344679d09c03d29fe9788edb8a31f
balls <dickn>
2022-07-01 00:41:43 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Destructive Bros game or whatever
+
+Basically a fort building game that will have support for online play.
+
+Problems:
+- Doesn't exit properly, not sure why, might be a python issue or a pygame issue but
+  calls are being properly called. 
---
+++ game.py
@@ -0,0 +1,102 @@
+#Multiplayer game stuff
+import pygame
+from pygame.locals import *
+import sys 
+
+pygame.init()
+
+vec = pygame.math.Vector2
+HEIGHT = 450
+WIDTH = 400
+ACC = 0.5
+FRIC = -0.12
+FPS = 60
+
+FramePerSec = pygame.time.Clock()
+
+displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("DestrBroGame")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((30,30))
+        self.surf.fill((128,255,40))
+        self.rect = self.surf.get_rect()
+
+        self.pos = vec((10, 10))
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+
+    def move(self):
+        self.acc = vec(0,0.5)
+
+        pressed_keys = pygame.key.get_pressed() 
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+                self.vel.y = 0
+            self.acc.x = ACC
+
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+    
+        self.rect.midbottom = self.pos
+
+    def update(self):
+        hits = pygame.sprite.spritecollide(p1, platforms, False)
+        if p1.vel.y > 0:
+            if hits:
+                self.vel.y = 0
+                self.pos.y = hits[0].rect.top + 1
+
+    def jump(self):
+        hits = pygame.sprite.spritecollide(self, platforms, False)
+        if hits:
+            self.vel.y = -15
+
+class platform(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 20))
+        self.surf.fill((255,0,0))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
+
+plat = platform()
+p1 = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(plat)
+all_sprites.add(p1)
+
+platforms = pygame.sprite.Group()
+platforms.add(plat)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                p1.jump()
+                    
+    displaysurface.fill((0,0,0))
+    
+    p1.move()
+    p1.update()
+
+    for entity in all_sprites:
+        displaysurface.blit(entity.surf, entity.rect)
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
+
+pygame.quit()
+sys.exit()
